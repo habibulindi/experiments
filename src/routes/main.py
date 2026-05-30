69d7360b922cdbfa117d7e01dc4cca3c87350dfd
habibulindi <dimochka.khabibulin@mail.ru>
2026-05-30 18:11:50 +0500

    Update main.py

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,10 +1,6 @@
 from src.app import app
 
+
 @app.route("/")
 def home():
     return {"status": "ok"}
-
-def register_routes(app):
-    @app.route("/")
-    def home():
-        return {"status": "ok"}
